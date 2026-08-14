8c46b1dc6ea1a019f3f5e54bcc3686433555af98
Suraj Mahto <surajcbsa06@gmail.com>
2026-08-15 00:49:03 +0530

    Time: 51 ms (22.33%) | Memory: 54.3 MB (25.53%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+//  Done
+
+var mergeAlternately = function(word1, word2) {
+    
+    let result = ""
+    for(let i=0; i< word1.length + word2.length ; i++){
+         if(i<word1.length){
+            result += word1[i]
+         }
+         if(i<word2.length){
+            result += word2[i]
+         }
+    }
+    return result
+    
+};
