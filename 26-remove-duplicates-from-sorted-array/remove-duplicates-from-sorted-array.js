c3a262d8ba237a49066461ae00d5fa09577754b3
Suraj Mahto <surajcbsa06@gmail.com>
2026-08-28 12:41:22 +0530

    Time: 1 ms (48.93%) | Memory: 58.4 MB (26.11%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let k =1
-    for(let i=1; i<nums.length; i++){
-          if(nums[i] !== nums[i-1]){
-             nums[k] = nums[i]
-             k++
-          }
+    let x =0
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] > nums[x]){
+          x += 1
+          nums[x] = nums[i]
+        }
     }
-    return k
+return x +1
 };
